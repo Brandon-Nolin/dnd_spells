93d4aff2f614c79fd5c0dd708de1b432cf0b152b
Brandon Nolin <brandonolinn@gmail.com>
2024-02-08 15:37:09 -0600

    Adds search elements to header and search route.
    Added elements for searching by name, class, and school to the header.
    Also created a search route for spells.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   root to: "home#index"
   get 'about', to: "about#index"
 
-  resources :spells, only: [:index, :show]
+  resources :spells, only: [:index, :show] do
+    collection do
+      get "search"
+    end
+  end
+
   resources :character_classes, only: [:index, :show]
   resources :schools, only: [:index, :show]
 end
